b38321836cc204dde81077e9cfe85c1c12a35163
Olexandr Kushnir <olexandrrazor@mail.ru>
2016-06-06 23:26:42 +0300

    badChanges

--- smartHouse/Models/AccountViewModels.cs
+++ smartHouse/Models/AccountViewModels.cs
@@ -3,25 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace smartHouse.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
-
-    public class ExternalLoginListViewModel
-    {
-        public string ReturnUrl { get; set; }
-    }
-
-    public class SendCodeViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
-        public string ReturnUrl { get; set; }
-        public bool RememberMe { get; set; }
-    }
+    
 
     public class VerifyCodeViewModel
     {
